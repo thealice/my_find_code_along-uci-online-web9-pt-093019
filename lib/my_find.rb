9f94c7c709abc5c12c5ffe319830cdb862f21807
Alice Freda <alicefreda@gmail.com>
2019-10-16 20:09:34 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,9 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-  i = 0
-  while i < collection.length
-    return yield(collection[i]) if yield(collection[i])
-    i += 1
-  end
+
+    i = 0
+    while i < collection.length
+      return collection[i] if yield(collection[i])
+      i = i + 1
+    end
+
 end
